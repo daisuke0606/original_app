5cfda579417c849ba7c212cbe1a08b19c866f820
daisuke0606 <68953503+daisuke0606@users.noreply.github.com>
2020-09-09 11:51:12 +0900

    active hashの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,30 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_days
   belongs_to_active_hash :trader_name
 
+  NAME_HARF_NAMBER = /\A[0-9]+\z/.freeze
+
+  with_options presence: true do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefectures_id
+    validates :shipping_days_id
+    validates :info
+    validates :name
+    validates :price, numericality: { greater_than: 299, less_than: 10_000_000 },
+                      format: { with: NAME_HARF_NAMBER, message: 'には半角数字に設定してください' }
+    validates :image
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :shipping_days_id
+    validates :prefectures_id
+    validates :trader_name_id
+  end
+
   belongs_to :user
   has_one_attached :image
 end
