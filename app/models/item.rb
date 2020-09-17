14f8a4ca7c393fff1adb9d97c67f9ab8326decf7
daisuke0606 <68953503+daisuke0606@users.noreply.github.com>
2020-09-17 15:47:47 +0900

    validateの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :prefectures
@@ -10,20 +9,18 @@ class Item < ApplicationRecord
   NAME_HARF_NAMBER = /\A[0-9]+\z/.freeze
 
   with_options presence: true do
-    validates :category_id
     validates :status_id
     validates :delivery_fee_id
     validates :prefectures_id
     validates :shipping_days_id
     validates :info
-    validates :name
+    validates :buy
     validates :price, numericality: { greater_than: 299, less_than: 10_000_000 },
                       format: { with: NAME_HARF_NAMBER, message: 'には半角数字に設定してください' }
     validates :image
   end
 
   with_options numericality: { other_than: 0 } do
-    validates :category_id
     validates :status_id
     validates :delivery_fee_id
     validates :shipping_days_id
