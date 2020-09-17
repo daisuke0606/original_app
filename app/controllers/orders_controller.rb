bfbaf5490d590192319b01f73b9f1d7bf1e8ad43
daisuke0606 <68953503+daisuke0606@users.noreply.github.com>
2020-09-17 14:09:10 +0900

    未完成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,33 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:new, :create]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
 
   def new
+    # @order = OrderAddress.new
+  end
+
+  def create
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def order_params
+    params.permit(:item_id, :post_code, :prefectures_id, :city, :block, :building, :phone, :token).merge(user_id: current_user.id)
+  end
+
 
 end
