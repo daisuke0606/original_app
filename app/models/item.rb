853d9f27d106f95def78d3148f7ec2c01da5894a
daisuke0606 <68953503+daisuke0606@users.noreply.github.com>
2020-09-23 14:56:41 +0900

    モデルに記述追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,6 +29,9 @@ class Item < ApplicationRecord
     validates :trader_name_id
   end
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   belongs_to :user
   has_one :order
   has_many :comments, dependent: :destroy
